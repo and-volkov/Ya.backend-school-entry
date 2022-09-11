13cda5e704e5fb7ffc2433e995de1ef7f7325613
Andrey Volkov <andrey.dreamwall@gmail.com>
2022-09-11 19:07:26 +0300

    create db connection file

---
+++ backend/app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+from settings import db_settings
+
+load_dotenv()
+
+DB_URL = db_settings.uri
+
+engine = create_engine(DB_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
