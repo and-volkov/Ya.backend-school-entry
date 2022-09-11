3baf01dff949c471e80fe79ebc81f98ede16ac8c
Andrey Volkov <andrey.dreamwall@gmail.com>
2022-09-11 19:06:55 +0300

    create api and db settings

---
+++ backend/app/settings.py
@@ -0,0 +1,28 @@
+import pydantic
+
+
+class BaseSettings(pydantic.BaseSettings):
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+
+class ApiSettings(BaseSettings):
+    title: str = 'Yandex Backend school september 2022. Disk app'
+    host = 'HOST'
+    port = 80
+    log_level: str = 'INFO'
+
+    class Config:
+        env_prefix = 'API_'
+
+
+class DBSettings(BaseSettings):
+    uri = 'URI'
+
+    class Config:
+        env_prefix = 'DB_'
+
+
+api_settings = ApiSettings()
+db_settings = DBSettings()
