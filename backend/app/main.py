23f672a998c7415993b4a7bfca3f63ecb7679c83
Andrey Volkov <andrey.dreamwall@gmail.com>
2022-09-13 21:15:38 +0300

    create updates endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,27 @@
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException, status
-from sqlalchemy import event
 from fastapi.exceptions import ValidationError, RequestValidationError
+from sqlalchemy import event
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 
 from backend.app import models
+from backend.app import settings
 from backend.app.database import SessionLocal
-from backend.app.settings import api_settings
 from backend.app.exceptions import validation_error_handler
 from backend.app.handlers.base import NodeHandler
-from backend.app.handlers.events import update_size
+from backend.app.handlers.events import update_size_and_date
+from backend.app.validators import validate_date
 
-app = FastAPI()
+
+app = FastAPI(
+    title=settings.api_settings.title,
+    description=settings.api_settings.description,
+    responses={
+        status.HTTP_400_BAD_REQUEST: {'model': settings.ErrorResponse},
+    }
+)
+
 app.add_exception_handler(RequestValidationError, validation_error_handler)
 app.add_exception_handler(IntegrityError, validation_error_handler)
 app.add_exception_handler(ValidationError, validation_error_handler)
@@ -21,24 +30,22 @@ app.add_exception_handler(ValidationError, validation_error_handler)
 def get_db():
     db = SessionLocal()
     try:
-        event.listen(db, 'before_commit', update_size)
+        event.listen(db, 'before_commit', update_size_and_date)
         yield db
     finally:
         db.close()
 
 
-@app.get('/')
-def test():
-    return {'text': 'hello'}
-
-
-@app.post('/imports', status_code=200)
+@app.post('/imports')
 async def import_node(items: models.ImportNode, db: Session = Depends(get_db)):
     NodeHandler(db).insert_or_update_nodes(items)
     return status.HTTP_200_OK
 
 
-@app.get('/nodes/{id}', response_model=models.ResponseNode)
+@app.get('/nodes/{id}', response_model=models.ResponseNode,
+         responses={
+             status.HTTP_404_NOT_FOUND: {'model': settings.ErrorResponse}
+         })
 async def get_node(id: str, db: Session = Depends(get_db)):
     node = NodeHandler(db).get_node(id)
     if not node:
@@ -47,17 +54,25 @@ async def get_node(id: str, db: Session = Depends(get_db)):
 
 
 @app.delete('/delete/{id}')
-async def delete_node(id: str, db: Session = Depends(get_db)):
-    req = id.split('?')
-    node_id = req[0]
-    return NodeHandler(db).delete_node(node_id)
+async def delete_node(
+    id: str, date: str = Depends(validate_date), db: Session = Depends(get_db)
+):
+    return NodeHandler(db).delete_node(id)
+
+
+@app.get('/updates', response_model=models.ResponseUpdates)
+async def get_updates(
+    date: str = Depends(validate_date), db: Session = Depends(get_db)
+):
+    updates = NodeHandler(db).get_node_updates(date)
+    return models.ResponseUpdates(items=updates)
 
 
 if __name__ == '__main__':
     uvicorn.run(
         'main:app',
-        host=api_settings.host,
-        port=api_settings.port,
-        log_level=api_settings.log_level.lower(),
+        host=settings.api_settings.host,
+        port=settings.api_settings.port,
+        log_level=settings.api_settings.log_level.lower(),
         reload=True,
     )
