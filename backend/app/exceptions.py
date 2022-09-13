9b243c081a53c739cb2ffd83d84cd770c8722d2e
Andrey Volkov <andrey.dreamwall@gmail.com>
2022-09-13 21:14:17 +0300

    refactor

--- backend/app/exceptions.py
+++ backend/app/exceptions.py
@@ -3,17 +3,14 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
+error_message = {'code': 400, 'message': 'Validation error'}
+
 
 async def validation_error_handler(
-    request: Request,
-    exc: RequestValidationError,
+        request: Request,
+        exc: RequestValidationError,
 ) -> JSONResponse:
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
-        content=jsonable_encoder(
-            {
-                'code': 400,
-                'message': 'Validation error',
-            }
-        ),
+        content=jsonable_encoder(error_message),
     )
