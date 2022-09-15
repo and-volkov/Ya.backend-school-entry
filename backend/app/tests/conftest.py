571c31f760a2cb6229d02366250b0304ae62fbea
Andrey Volkov <andrey.dreamwall@gmail.com>
2022-09-15 16:15:13 +0300

    refactor

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -4,7 +4,7 @@ from fastapi.testclient import TestClient
 from backend.app.main import app
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='session')
 def test_app():
     client = TestClient(app)
     yield client
